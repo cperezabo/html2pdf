634aaef51f27216d261594ab2f620577262908ca
Cristián <cristian@ioduck.com>
2016-04-06 18:34:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# CLI HTML to PDF converter
+
+## About
+Just a simple CLI converter based on [gulp-html-pdf](https://www.npmjs.com/package/gulp-html-pdf)
+
+
+## Installation
+```
+$ npm install -g html2pdf-cli
+```
+
+## Usage
+```
+$ html2pdf src dest [options-file]
+```
+Example:
+```
+$ html2pdf file.html ./pdf  
+$ html2pdf ./path/**/*.html ./pdf ../options.json
+```
+
+## Options
+https://github.com/marcbachmann/node-html-pdf#options
+
+##Problems?
+[Open an Issue](https://github.com/cperezabo/html2pdf-cli/issues)
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var gulp = require('gulp'),
+    pdf = require('gulp-html-pdf');
+
+var args = process.argv.slice(2)
+
+if(args.length >= 2) {
+    options = {}
+
+    if(args[2]) {
+        options = require(args[2])
+    }
+
+    html2pdf(args[0], args[1], options)
+} else {
+    help()
+}
+
+function help() {
+    var help = [
+        'Usage: html2pdf <src> <dest> <options-file>',
+        'e.g.:',
+        'html2pdf file.html ./pdf',
+        'html2pdf ./path/**/*.html ./pdf ../options.json'
+    ].join('\n')
+
+    console.log(help)
+}
+
+function html2pdf(src, dest, options) {
+    gulp.src(src)
+        .pipe(pdf(options))
+        .pipe(gulp.dest(dest))
+}
